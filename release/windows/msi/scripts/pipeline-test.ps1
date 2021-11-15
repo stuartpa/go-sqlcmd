9f25d690acd103ea30c59238f529e16a06bb69ff
Stuart Padley <stuartpa@microsoft.com>
2021-11-15 12:55:43 -0800

    .msi

---
+++ release/windows/msi/scripts/pipeline-test.ps1
@@ -0,0 +1,38 @@
+#------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#------------------------------------------------------------------------------
+
+# Description:
+#
+# Instructions to be invoked under the build CI pipeline in AzureDevOps.
+#
+# Kickoff MSI package install test.
+#
+# Usage:
+#
+# set SYSTEM_ARTIFACTSDIRECTORY=\path\to\msi\go-mssqltools-<CL_VERSION>.msi
+#
+# $ pipeline-test.ps1
+        
+$msiPath = Join-Path $env:SYSTEM_ARTIFACTSDIRECTORY "msi\go-mssqltools.msi"
+
+$InstallArgs = @(
+    "/I"
+    $msiPath
+    "/norestart"
+    "/L*v"
+    ".\install-logs.txt"
+    "/qn"
+)
+        
+Write-Output "Starting msi install $msiPath..."
+Start-Process "msiexec.exe" -ArgumentList $InstallArgs -Wait -NoNewWindow
+Get-Content .\install-logs.txt
+
+Write-Output "Done installing msi, checking PATH setup..."
+Write-Output "$env:path"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+Write-Output "$env:path"
+
+sqlcmd
+sqlcmd --version
